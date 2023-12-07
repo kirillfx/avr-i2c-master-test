504f16b71ac29453893edc486de1dd61ab77cae0
Kirill Chayka <kirillfx@gmail.com>
2023-12-07 18:55:40 +0200

    chore: cleanup

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ use ufmt::uwriteln;
 
 static BTN_FLAG: AtomicBool = AtomicBool::new(false);
 
+// Attach btn interrupt handler connected to pin d2
 #[avr_device::interrupt(atmega328p)]
 fn INT0() {
     avr_device::interrupt::free(|_| {
@@ -27,30 +28,15 @@ fn main() -> ! {
 
     unsafe { avr_device::interrupt::enable() };
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
-
     let mut led = pins.d13.into_output();
 
-    let btn_pin = pins.d2.into_pull_up_input();
+    // Use internal pullup resistor
+    let _btn_pin = pins.d2.into_pull_up_input();
 
     dp.EXINT.eicra.modify(|_, w| w.isc0().bits(0x03));
     dp.EXINT.eimsk.modify(|_, w| w.int0().set_bit());
 
-    // let mut i2c = arduino_hal::I2c::new(
-    //     dp.TWI,
-    //     pins.a4.into_pull_up_input(),
-    //     pins.a5.into_pull_up_input(),
-    //     100000,
-    // );
-
+    // Configure with external pullup resistors. I use a pair of 1k8
     let mut i2c = arduino_hal::I2c::with_external_pullup(
         dp.TWI,
         pins.a4.into_floating_input(),
@@ -58,30 +44,27 @@ fn main() -> ! {
         100000,
     );
 
-    led.set_low();
-
-    arduino_hal::delay_ms(1000);
-    led.set_high();
-
-    let addr: u8 = 38;
+    let addr: u8 = 0x26;
     let global_addr: u8 = 0x00;
 
     uwriteln!(&mut serial, "Initialized").unwrap();
 
-    let b: [u8; 1] = [0; 1];
-
-    // i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Write);
-    // i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Read);
+    let _ = i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Write);
+    let _ = i2c.i2cdetect(&mut serial, arduino_hal::i2c::Direction::Read);
 
     loop {
         if BTN_FLAG.load(Ordering::SeqCst) {
-            BTN_FLAG.store(false, Ordering::SeqCst);
+            led.set_high();
 
-            uwriteln!(&mut serial, "Writing to i2c at {}", addr).unwrap();
+            uwriteln!(&mut serial, "Writing to i2c address: 0x{:X}", addr).unwrap();
             let b = [207, 206];
-            if let Err(err) = i2c.write(addr, &b) {
-                uwriteln!(&mut serial, "I2C error: {:?}", err as I2CError).unwrap();
+            match i2c.write(addr, &b) {
+                Err(err) => uwriteln!(&mut serial, "I2C error: {:?}", err as I2CError).unwrap(),
+                Ok(_) => uwriteln!(&mut serial, "Done\n").unwrap(),
             };
+
+            led.set_low();
+            BTN_FLAG.store(false, Ordering::SeqCst);
         }
 
         // {
